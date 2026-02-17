ae1604a753ef29de622f4b1aa07ded0fb1fee021
VISHAL P <pvishalvishal29iot24-28@ksrce.ac.in>
2026-02-17 19:08:49 +0530

    Add files via upload

---
+++ Day5I.py
@@ -0,0 +1,3 @@
+fruits=["Apple","Orange","Banana"]
+for fruit in fruits:
+    print(fruit)
---
+++ Day5II.py
@@ -0,0 +1,7 @@
+numbers=[123,4534,56,5,33,324,6576,3,4545,23,5,34,2,1,123,45,56,5766,7688,8,9,90,6]
+
+max=0
+for number in numbers:
+    if number>max:
+        max=number
+print(max)
